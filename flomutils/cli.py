b7fe04c441dd1e12989ffeff8f45604929f309b5
coord.e <me@coord-e.com>
2019-01-17 23:54:09 +0000

    Add: Add add_noise command

--- flomutils/cli.py
+++ flomutils/cli.py
@@ -14,3 +14,12 @@ def plot(motion, output, fps=0.01, loop=1):
 
     ax.legend()
     plt.savefig(output)
+
+
+def add_noise(motion, output, random=0.1):
+    from flomutils import noise
+
+    input_motion = flom.load(motion)
+
+    noise.add_noise(input_motion, random)
+    input_motion.dump(output)
